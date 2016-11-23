ede59c1a152692d4386fd8d44615444455156370
Phyc4s <phyc4s@gmail.com>
2016-11-23 14:53:52 -0300

    listo el thread para la telemetría

---
+++ HazeTel/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace HazeTel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tempLabel = new System.Windows.Forms.Label();
+            this.loadLabel = new System.Windows.Forms.Label();
+            this.upTimeLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tempLabel
+            // 
+            this.tempLabel.AutoSize = true;
+            this.tempLabel.Location = new System.Drawing.Point(119, 84);
+            this.tempLabel.Name = "tempLabel";
+            this.tempLabel.Size = new System.Drawing.Size(26, 13);
+            this.tempLabel.TabIndex = 1;
+            this.tempLabel.Text = "0 c°";
+            // 
+            // loadLabel
+            // 
+            this.loadLabel.AutoSize = true;
+            this.loadLabel.Location = new System.Drawing.Point(141, 127);
+            this.loadLabel.Name = "loadLabel";
+            this.loadLabel.Size = new System.Drawing.Size(21, 13);
+            this.loadLabel.TabIndex = 2;
+            this.loadLabel.Text = "0%";
+            // 
+            // upTimeLabel
+            // 
+            this.upTimeLabel.AutoSize = true;
+            this.upTimeLabel.Location = new System.Drawing.Point(141, 242);
+            this.upTimeLabel.Name = "upTimeLabel";
+            this.upTimeLabel.Size = new System.Drawing.Size(49, 13);
+            this.upTimeLabel.TabIndex = 3;
+            this.upTimeLabel.Text = "00:00:00";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(102, 366);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 425);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.upTimeLabel);
+            this.Controls.Add(this.loadLabel);
+            this.Controls.Add(this.tempLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label tempLabel;
+        private System.Windows.Forms.Label loadLabel;
+        private System.Windows.Forms.Label upTimeLabel;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ telemetry/Class1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//NEW
+using OpenHardwareMonitor;
+using System.Diagnostics;
+using OpenHardwareMonitor.Hardware;
+using System.Speech.Synthesis;
+
+namespace telemetry
+{
+    public class Telemetry
+    {
+        public static PerformanceCounter PC_uptime = new PerformanceCounter("System", "System Up Time"); //uptime from the Performance Counter of windows
+        public static Computer pc;
+
+        public Telemetry()
+        {
+            pc = new Computer() //Creating the computer object for readings
+             {
+                 CPUEnabled = true, //initialize the CPU
+                 GPUEnabled = true //initialize the GPU
+             };
+            PC_uptime.NextValue(); //the method will activate all this things and then start the thread that actually prints the info
+            pc.Open();
+        }
+        public string UpTime() //Returns a string with the uptime
+        {
+            TimeSpan uptimeSpan = TimeSpan.FromSeconds(PC_uptime.NextValue()); // convert uptime to seconds
+            string timeawake = string.Format("Up time: {0} days {1}:{2}:{3}", //store uptime formated into a string
+                (int)uptimeSpan.TotalDays, //this stores the total of days
+                (int)uptimeSpan.Hours,      //this stores the hours since last day
+                (int)uptimeSpan.Minutes,    //this stores the minutes passed since last hour
+                (int)uptimeSpan.Seconds        // this stores the seconds since last minute
+                );
+
+            return timeawake;
+        }
+
+        public float CPU_load() //float with the load of the CPU
+        {
+            float load = 0;
+            try
+            {
+                #region //foreach thingy things
+                foreach (var hardwareItem in pc.Hardware)  //store the hardware in hardwareItem
+                {
+
+                    if (hardwareItem.HardwareType == HardwareType.CPU) //Select the CPU of the hardware list
+                    {
+                        hardwareItem.Update(); //call the update (i don't know what this is actually
+                        foreach (var sensorItem in hardwareItem.Sensors) //store every sensor of the CPU on sensorItem
+                        {
+                            if (sensorItem.SensorType == SensorType.Load) //If the sensorItem is a temperature sensor , prints the value OR the default
+                            {
+                                load = sensorItem.Value.GetValueOrDefault();
+                            }
+                        }
+                    }
+                }
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                load = 0;
+               /* Console.WriteLine("ERROR READING CPU LOAD");
+                Console.WriteLine("SOMETHING WENT AWFULLY WRONG");
+                Console.WriteLine(ex.Message);*/
+            }
+
+            return load;
+
+        }
+
+
+        public float CPU_temp() //float with the temperature of the CPU
+        {
+            float temp = 0;
+            try
+            {
+                #region //foreach thingy things
+                foreach (var hardwareItem in pc.Hardware)  //store the hardware in hardwareItem
+                {
+
+                    if (hardwareItem.HardwareType == HardwareType.CPU) //Select the CPU of the hardware list
+                    {
+                        hardwareItem.Update(); //call the update (i don't know what this is actually
+                        foreach (var sensorItem in hardwareItem.Sensors) //store every sensor of the CPU on sensorItem
+                        {
+                            if (sensorItem.SensorType == SensorType.Temperature) //If the sensorItem is a temperature sensor , prints the value OR the default
+                            {
+                                temp = sensorItem.Value.GetValueOrDefault();
+                            }
+                        }
+                    }
+                }
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                temp = 69;
+                /*
+                Console.WriteLine("ERROR READING CPU TEMPERATURE");
+                Console.WriteLine("MAKE SURE THE APP HAS ADMIN RIGHTS");
+                Console.WriteLine(ex.Message);*/
+            }
+
+            return temp;
+        }
+
+
+        public class voice
+        {
+            public static SpeechSynthesizer synth = new SpeechSynthesizer();
+            public static void Speak(string message, int rate) //speak the message at the desired speed (rate)
+            {
+                synth.Volume = 100;
+                synth.Rate = rate;
+                synth.Speak(message);
+            }
+
+        }
+    }
+}
